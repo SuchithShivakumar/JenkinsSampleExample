2c86082f4a845a6d21f2fc0c6b5b4a87620ddf54
SuchithShivakumar <suchith.shivkumar@gmail.com>
2019-05-25 22:47:59 -0500

    Changes to HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,11 @@ public class HelloWorld{
 	
 	public static void main(String[] args){
 	
+		System.out.println("Running In Jenkins");
 		for(int i=1;i<=10;i++){
 			System.out.println("Hello World ..."+i);
 		
 		}
-	
+		System.out.println("Succesfully);
 	}
 }
